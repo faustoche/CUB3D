061c995fe08e333c92912372a869f13a8cbbaebf
faustoche <faustine.crocq@gmail.com>
2025-05-21 22:53:46 +0200

    texturing

--- src/rendering/render_walls.c
+++ src/rendering/render_walls.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render_walls.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcrocq <fcrocq@student.42.fr>              +#+  +:+       +#+        */
+/*   By: faustoche <faustoche@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 16:55:06 by fcrocq            #+#    #+#             */
-/*   Updated: 2025/05/21 18:39:43 by fcrocq           ###   ########.fr       */
+/*   Updated: 2025/05/21 22:52:10 by faustoche        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,14 +60,56 @@ static int	get_color(t_mlx *mlx)
 	}
 }
 
-static void	draw_wall(t_mlx *mlx, int ray, int top_pixel, int bottom_pixel)
-{
-	int	color;
+// static void	draw_wall(t_mlx *mlx, int ray, int top_pixel, int bottom_pixel)
+// {
+// 	int	color;
 
-	color = get_color(mlx);
-	while (top_pixel < bottom_pixel)
-		ft_mlx_pixel_put(mlx, ray, top_pixel++, color);
-}
+// 	color = get_color(mlx);
+// 	while (top_pixel < bottom_pixel)
+// 		ft_mlx_pixel_put(mlx, ray, top_pixel++, color);
+// }
+
+// static void draw_textured_wall(t_mlx *mlx, int ray, int top_pixel, int bottom_pixel)
+// {
+//     int			y;
+//     int			tex_x;
+//     int			tex_y;
+//     double		step;
+//     double		tex_pos;
+//     int			wall_height;
+//     t_texture	*texture;
+// 	int			color;
+
+//     if (mlx->ray->flag == 1) // Mur vertical
+//     {
+//         if (cos(mlx->ray->ray_angle) > 0)
+//             texture = &mlx->game->west_texture;
+//         else
+//             texture = &mlx->game->east_texture;
+//         tex_x = (int)(mlx->ray->next_y) % TILE_SIZE;
+//     }
+//     else // Mur horizontal
+//     {
+//         if (sin(mlx->ray->ray_angle) > 0)
+//             texture = &mlx->game->north_texture;
+//         else
+//             texture = &mlx->game->south_texture;
+//         tex_x = (int)(mlx->ray->next_x) % TILE_SIZE;
+//     }
+//     tex_x = (tex_x * texture->width) / TILE_SIZE;
+//     wall_height = (int)((TILE_SIZE / mlx->ray->distance) * ((WIDTH / 2) / tan(mlx->player->fov_rd / 2)));
+//     step = (double)texture->height / wall_height;
+//     tex_pos = (top_pixel - HEIGHT / 2 + wall_height / 2) * step;
+//     y = top_pixel;
+//     while (y < bottom_pixel)
+//     {
+//         tex_y = (int)tex_pos & (texture->height - 1);
+//         tex_pos += step;
+// 		color = get_texture_pixel(texture, tex_x, tex_y);
+//         ft_mlx_pixel_put(mlx, ray, y, color);
+//         y++;
+//     }
+// }
 
 void	render_wall(t_mlx *mlx, int ray)
 {
@@ -84,5 +126,5 @@ void	render_wall(t_mlx *mlx, int ray)
 	if (top_pixel < 0)
 		top_pixel = 0;
 	draw_floor_ceiling(mlx, ray, top_pixel, bottom_pixel);
-	draw_wall(mlx, ray, top_pixel, bottom_pixel);
+	draw_textured_wall(mlx, ray, top_pixel, bottom_pixel);
 }
